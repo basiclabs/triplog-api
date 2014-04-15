e80dce352209eb79ecb0405012b26462829e07ed
Henry Richardson <henry.d.richardson@gmail.com>
2014-04-15 11:14:19 -0400

    Pretty self explained

---
+++ main.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+require 'data_mapper'
+require 'sqlite3'
+
+load 'Support_rb/api_endpoint.rb'
---
+++ support_rb/api_endpoints.rb
@@ -0,0 +1,29 @@
+# api_endpoints.rb
+
+get '/trips' do
+		'Hello world!'
+	end
+
+get '/trips/:id' do
+	end
+
+get '/trips/:id/photos' do
+	end
+
+post '/trips' do
+	end
+
+post '/trips/:id/photos' do
+	end
+
+delete '/trips/:id do
+	end
+
+delete '/trips/:id/photos/:id' do
+	end
+
+put '/trips/:id' do
+	end
+
+put '/trips/:id/photos/:id' do
+	end
