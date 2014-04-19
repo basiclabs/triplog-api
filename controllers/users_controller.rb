6d5284cbfe888c02ccb829c213e3e14b70e882ad
Zesty <superdeadman99@gmail.com>
2014-04-19 18:13:03 -0400

    Basically added basic user authentication, basically

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -25,14 +25,22 @@ end
 
 post '/register' do
 	if(params[:password] == params[:confirmPassword])
+		if (mUsers.where(:email => params[:email]).count > 0) #checks to see if email is already in db
+			redirect '/login' #TODO: update this when login updates
+		else
 			mUsers.insert(:name => params[:name], :password => params[:password], :email => params[:email])
-		session[:user] = true
-		redirect '/'
+			session[:user] = true
+			redirect '/'
+		end
 	else
 		'Passwords must match'
 	end
 end
 
 post '/login' do
-	'hi'
+	if(mUsers.where(:name => params[:name], :password => params[:password]).count > 0) #checks if name and password are in database
+		'Yahello senpai~<3~'
+	else
+		redirect '/'
+	end
 end
