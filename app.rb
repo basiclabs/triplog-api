e3b2a72c948d15bdc37f96189f691f53b575aa56
TomaQ <thomasiscooly@gmail.com>
2014-04-18 20:37:49 -0400

    updated app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,49 @@
+require 'rubygems'
+require 'sinatra'
 require 'sequel'
-DB = Sequel.connect('sqlite://dev.db')
-
 require './models/trip'
+
+enable :sessions
+
+DB = Sequel.sqlite('dev.db')
+
+mUsers = DB[:users]
+
+get '/' do
+    erb :index
+end
+
+get '/register' do
+    erb :register
+end
+
+get '/login' do
+    erb :login
+end
+
+get '/logout' do
+    session.clear
+    redirect '/'
+end
+
+get '/test' do
+	checkUsers = DB[:users]
+	DB[:users].filter(:name=>'Peep').map(:name).join(', ')
+end	
+
+post '/register' do
+	if(params[:password] == params[:confirmPassword])
+
+		if(checkUsers.all != params[:name] && )
+			mUsers.insert(:name => params[:name], :password => params[:password], :email => params[:email])
+		#end
+		session[:user] = true
+		redirect '/'
+	else
+		'Passwords must match'
+	end
+end
+
+post '/login' do
+	'hi'
+end
