bc06c1c09bf5be3b1af8667305dff517ec5c690f
TomaQ <thomasiscooly@gmail.com>
2014-04-19 19:04:14 -0400

    made it work

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -2,6 +2,12 @@ enable :sessions
 
 mUsers = DB[:users]
 
+before do
+	if(session[:user_id])
+		@user = mUsers[:id => session[:user_id]]
+	end
+end
+
 get '/' do
     erb :index
 end
@@ -24,8 +30,7 @@ post '/register' do
 		if (mUsers.where(:email => params[:email]).count > 0) #checks to see if email is already in db
 			redirect '/login' #TODO: update this when login updates
 		else
-			mUsers.insert(:name => params[:name], :password => params[:password], :email => params[:email])
-			session[:user] = true
+			session[:user_id] = mUsers.insert(:name => params[:name], :password => params[:password], :email => params[:email])
 			redirect '/'
 		end
 	else
@@ -34,10 +39,11 @@ post '/register' do
 end
 
 post '/login' do
-	if(mUsers.where(:name => params[:name], :password => params[:password]).count > 0) #checks if name and password are in database
+	if(mUsers.where(:email => params[:email], :password => params[:password]).count > 0) #checks if name and password are in database
 		'Yahello senpai~<3~'
-		session[:user] = true
-	else
+		session[:user_id] = mUsers[:email => params[:email]][:id]
 		redirect '/'
+	else
+		redirect '/login'
 	end
 end
