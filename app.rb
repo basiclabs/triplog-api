33146f068351c73445d9dea78df00fdf1918c6eb
hkal <_@hkal.me>
2014-04-23 01:12:44 -0400

    use modular application structure:   * encapsulate relevant pieces inside a module   * inherit app from Sinatra::Application

--- app.rb
+++ app.rb
@@ -7,3 +7,10 @@ DB = Sequel.connect('sqlite://dev.db')
 require './models/trip'
 require './controllers/trips_controller'
 require './controllers/users_controller'
+
+module TripLog
+  class App < Sinatra::Application
+    use Controllers::Trips
+    use Controllers::Users
+  end
+end
--- controllers/trips_controller.rb
+++ controllers/trips_controller.rb
@@ -1,28 +1,34 @@
-get '/trips' do
-  Trip.all.to_json
-end
+module TripLog
+  module Controllers
+    class Trips < Sinatra::Application
+      get '/trips' do
+        Trip.all.to_json
+      end
 
-get '/trips/:id' do
-  Trip.where(:id => params[:id]).all.to_json
-end
+      get '/trips/:id' do
+        Trip.where(:id => params[:id]).all.to_json
+      end
 
-get '/trips/:id/photos' do
-end
+      get '/trips/:id/photos' do
+      end
 
-post '/trips' do
-end
+      post '/trips' do
+      end
 
-post '/trips/:id/photos' do
-end
+      post '/trips/:id/photos' do
+      end
 
-delete '/trips/:id' do
-end
+      delete '/trips/:id' do
+      end
 
-delete '/trips/:id/photos/:id' do
-end
+      delete '/trips/:id/photos/:id' do
+      end
 
-put '/trips/:id' do
-end
+      put '/trips/:id' do
+      end
 
-put '/trips/:id/photos/:id' do
+      put '/trips/:id/photos/:id' do
+      end
+    end
+  end
 end
